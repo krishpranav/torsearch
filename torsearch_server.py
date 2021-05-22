32a0575b713ea5515ae65efa42b284dcce2a4b84
krishpranav <krisna.pranav@gmail.com>
2021-05-22 13:32:10 +0530

    [+] updated [+]

---
+++ torsearch_server.py
@@ -0,0 +1,12 @@
+from torsearch.torsearch import app as application
+
+def main():
+    application.run(
+        host='0.0.0.0',
+        port=80,
+        threaded=True,
+        debug=True
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ torsearch_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import subprocess
+from time import sleep
+import requests
+
+process = subprocess.Popen("sudo gunicorn --bind 0.0.0.0:80 dark_server", shell=True)
+print('TorSearch started.')
+sleep(5)
+#  Check maine search screen
+darkRequest = requests.get('http://0.0.0.0')
+darkRequest.raise_for_status()
+#  Check cached pages get displayed
+process.kill()
+print('Darksearch is running in the background...')
